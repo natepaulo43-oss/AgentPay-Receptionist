8ea9613a601b5dcbc0f2171d83f5fd7384bc7faa
Laird Miller <laird.loera@gmail.com>
2026-05-06 11:17:47 -0400

    initial commit

--- dist/src/edge/shared/to-lambda-response.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CloudFrontResultResponse } from 'aws-lambda';
-export declare function toLambdaResponse(instructions: {
-    status: number;
-    headers: Record<string, string>;
-    body?: unknown;
-}): CloudFrontResultResponse;
-//# sourceMappingURL=to-lambda-response.d.ts.map
